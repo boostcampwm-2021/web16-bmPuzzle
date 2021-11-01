3ac6728a4a85bd6a074fa83ffb220bfca0eb41d9
Yu Jin <susanna1221@naver.com>
2021-11-01 15:35:11 +0900

    feat: Header 마크업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
-
-function App() {
-  return <div>hello</div>;
-}
-
+import React, { useState, useRef, useCallback } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Main} exact={true} />
+        <Route path="/regpuz" component={Space} exact={true} />
+        render=
+        {({ location: any }) => (
+          <div>
+            <h1>ERROR</h1>
+            <h2>이 페이지는 존재하지 않습니다.</h2>
+          </div>
+        )}
+      </Switch>
+    </Router>
+  );
+};
 export default App;
