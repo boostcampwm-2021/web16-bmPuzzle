ac96ac6b0304ec4be2f1b663b286830b298a75c0
JaeYeong-Li <akvk98@naver.com>
2021-11-01 18:22:03 +0900

    feat: global CSS 설정

--- frontend/src/Components/RegPuz/index.tsx
+++ frontend/src/Components/RegPuz/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import LevelComponent from "./Components/LevelComponent";
 import GlobalStyles from "../../GlobalStyles";
+import Header from "../Header";
 
 const RegPuzDiv = styled.div`
   .rp__title {
@@ -14,25 +15,30 @@ const RegPuzDiv = styled.div`
 
 const RegPuz: React.FC = () => {
   return (
-    <RegPuzDiv>
-      <GlobalStyles />
-      <div className="rp__title">
-        <p>Title</p>
-        <input type="text" name="title" />
+    <div id="wrapper">
+      <Header></Header>
+      <div id="body">
+        <RegPuzDiv>
+          <GlobalStyles />
+          <div className="rp__title">
+            <p>Title</p>
+            <input type="text" name="title" />
+          </div>
+          <div className="rp__upload">
+            <input type="file" name="img" accept="image/*" />
+          </div>
+          <div className="rp__level">
+            <div>Level</div>
+            <div className="rp__levelBox">
+              <LevelComponent num={1}></LevelComponent>
+              <LevelComponent num={2}></LevelComponent>
+              <LevelComponent num={3}></LevelComponent>
+            </div>
+          </div>
+          <div className="rp__submit"></div>
+        </RegPuzDiv>
       </div>
-      <div className="rp__upload">
-        <input type="file" name="img" accept="image/*" />
-      </div>
-      <div className="rp__level">
-        <div>Level</div>
-        <div className="rp__levelBox">
-          <LevelComponent num={1}></LevelComponent>
-          <LevelComponent num={2}></LevelComponent>
-          <LevelComponent num={3}></LevelComponent>
-        </div>
-      </div>
-      <div className="rp__submit"></div>
-    </RegPuzDiv>
+    </div>
   );
 };
 
