04fbabccb29ebcc38c3f75b10339e2cfe834ec11
Yu Jin <susanna1221@naver.com>
2021-11-01 17:25:59 +0900

    fix: 머지 충돌 해결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import React, { useState, useRef, useCallback } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import RegPuz from "@Components/RegPuz/index";
-import Header from "./Components/Header/index";
+import RegPuz from "./Components/RegPuz/index";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/temp_header" component={Header} exact={true} />
+        <Route path="/regpuz" component={RegPuz} exact={true} />
+        render=
+        {/* {({ location: any }) => (
+          <div>
+            <h1>ERROR</h1>
+            <h2>이 페이지는 존재하지 않습니다.</h2>
+          </div>
+        )} */}
       </Switch>
     </Router>
   );
