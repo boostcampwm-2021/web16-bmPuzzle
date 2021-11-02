efe0ef5705f6e769af905fc680561e09c5d03ab6
betty214 <bettyjang@ewhain.net>
2021-11-02 17:29:41 +0900

    feat:이미지 불러오기 캐쉬 구현 backend public의 이미지를 blob으로 불러오고 이미 불러온 img는 또 받지 않고 cache 처리

--- backend/api/search/search.ts
+++ backend/api/search/search.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import searchService from '@services/search-service';
+import sendImgUrl from '@services/search-service';
 const router = express.Router();
 
-router.get('/', searchService);
+router.get('/', searchService.search, searchService.sendImgUrl);
 
 export default router;
