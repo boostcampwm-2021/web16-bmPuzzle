7bbce8362ea289360c12f04b3d9126cfdf2b30c3
JaeYeong-Li <akvk98@naver.com>
2021-11-02 13:43:30 +0900

    feat: 체크박스 체크 하나만 되로록 구현
    useState 사용

--- frontend/src/components/register-puzzle/index.tsx
+++ frontend/src/components/register-puzzle/index.tsx
@@ -1,41 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import LevelComponent from "@components/register-puzzle/level-component";
 import GlobalStyles from "@styles/global-style";
 import Header from "@components/header/index";
 
 const RegPuzDiv = styled.div`
-  .rp__title {
+  .rp-title {
     display: flex;
   }
-  .rp__levelBox {
+  .rp-levelbox {
     display: flex;
   }
 `;
 
 const RegPuz = () => {
+  const [checkedLevel, setLevel] = useState(1);
   return (
     <div id="wrapper">
       <Header></Header>
       <div id="body">
         <RegPuzDiv>
           <GlobalStyles />
-          <div className="rp__title">
-            <p>Title</p>
-            <input type="text" name="title" />
-          </div>
-          <div className="rp__upload">
-            <input type="file" name="img" accept="image/*" />
-          </div>
-          <div className="rp__level">
-            <div>Level</div>
-            <div className="rp__levelBox">
-              <LevelComponent num={1}></LevelComponent>
-              <LevelComponent num={2}></LevelComponent>
-              <LevelComponent num={3}></LevelComponent>
+          <form
+            action="http://localhost:5000/api/register"
+            accept-charset="utf-8"
+            method="post"
+          >
+            <div className="rp-title">
+              <p>Title</p>
+              <input type="text" name="title" />
             </div>
-          </div>
-          <div className="rp__submit"></div>
+            <div className="rp-upload">
+              <input type="file" name="img" accept="image/*" />
+            </div>
+            <div className="rp-level">
+              <div>Level</div>
+              <div className="rp-levelbox">
+                <LevelComponent
+                  num={1}
+                  checkedLevel={checkedLevel}
+                  checkFunction={setLevel}
+                ></LevelComponent>
+                <LevelComponent
+                  num={2}
+                  checkedLevel={checkedLevel}
+                  checkFunction={setLevel}
+                ></LevelComponent>
+                <LevelComponent
+                  num={3}
+                  checkedLevel={checkedLevel}
+                  checkFunction={setLevel}
+                ></LevelComponent>
+              </div>
+            </div>
+            <div className="rp-submit">
+              <button type="submit">submit</button>
+            </div>
+          </form>
         </RegPuzDiv>
       </div>
     </div>
