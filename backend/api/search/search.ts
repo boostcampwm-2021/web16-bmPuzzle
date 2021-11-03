42bd07d234af51227558a6688748b6e53d2a4d3f
Kim-Ha-Jeong <kha031821@gmail.com>
2021-11-03 14:32:11 +0900

    feat: 오류 수정

--- backend/api/search/search.ts
+++ backend/api/search/search.ts
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.get('/', searchService.search, searchService.sendImgUrl);
 
-router.post('/', searchService.search, searchService.filterImgUrl);
+router.post('/', searchService.search, searchService.sendImgUrl);
 
 export default router;
--- backend/services/search-service.ts
+++ backend/services/search-service.ts
@@ -1,3 +1,4 @@
+import searchService from '@services/db/puzzle-service';
 import puzzleService from '@services/db/puzzle-service';
 import path from 'path';
 import fs from 'fs';
@@ -9,28 +10,23 @@ const search = (req: any, res: any, next: any) => {
 };
 
 const sendImgUrl = async (req: any, res: any) => {
-  const puzzles =await puzzleService.getPuzzle()
-  const returnData = req.files.map((file: any) => {
-    return puzzles.info.filter((puzzle: any) => puzzle.image == file)[0];
+  const puzzle =
+    req.body.keyword !== undefined
+      ? await searchService.filterPuzzle(req.body.keyword)
+      : await searchService.getPuzzle();
+  const filterInfo: any = [];
+  const fileName: any = [];
+  req.files.forEach((file: any) => {
+    const puzzleInfo = puzzle.info.filter(
+      (puzzle: any) => puzzle.image == file,
+    )[0];
+    puzzleInfo !== undefined
+      ? (filterInfo.push(puzzleInfo), fileName.push(puzzleInfo.image))
+      : '';
   });
-
   res.status(200).json({
-    data: returnData,
-    file_name: req.files,
+    data: filterInfo,
+    fileName: fileName,
   });
 };
-
-const filterImgUrl = async (req: any, res: any) => {
-    const puzzle = await puzzleService.filterPuzzle(req.body.keyword);
-    const filterInfo:any = [];
-    const fileName:any = [];
-    req.files.forEach((file:any) => {
-        const puzzleInfo = puzzle.info.filter((puzzle:any) => puzzle.image == file)[0];
-        puzzleInfo !== undefined ? (filterInfo.push(puzzleInfo), fileName.push(puzzleInfo.image)) : '';
-    })
-    res.status(200).json({
-        data: filterInfo,
-        fileName: fileName
-    })
-}
-export default { search, sendImgUrl, filterImgUrl };
+export default { search, sendImgUrl };
