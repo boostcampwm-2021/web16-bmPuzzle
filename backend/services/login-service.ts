1981ece9281bd9d35f4ebc690f4bcfabd5016ce1
Kim-Ha-Jeong <kha031821@gmail.com>
2021-11-08 16:53:06 +0900

    feat: img 로딩 속도 개선

--- backend/services/login-service.ts
+++ backend/services/login-service.ts
@@ -2,7 +2,7 @@ import userService from '@services/db/user-service';
 
 const loginService = async (req:any, res:any, next:any) => {
     const id = req.body.id;
-    const statusCode = await userService(id);
+    const statusCode = await userService.postData(id);
 
     res.status(statusCode.code).json(statusCode.msg);
 }
