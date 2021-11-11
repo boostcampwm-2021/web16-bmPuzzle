1f4ecb86e55a9a0ddce28d6f00ebb734ff7ab087
JaeYeong-Li <akvk98@naver.com>
2021-11-11 12:34:45 +0900

    feat: 조회수 증가 기능

--- backend/services/play-puzzle.ts
+++ backend/services/play-puzzle.ts
@@ -1,11 +1,14 @@
-import searchService from '@services/db/puzzle-service';
+import puzzleService from '@services/db/puzzle-service';
 
 const roomURL = new Set<string>();
 
 const getPuzzleInfo = async (req: any, res: any, next: any) => {
-  const puzzleInfo = await searchService.getPuzzleByID(req.params.puzzleID);
+  const puzzleID = Number(req.params.puzzleID);
+  const puzzleInfo = await puzzleService.getPuzzleByID(puzzleID);
   if (puzzleInfo.code === 500) res.status(500);
   else {
+    const updatedVisitTime = await puzzleService.updateVisitTime(puzzleID);
+    if (updatedVisitTime.code === 500) console.log('조회수 증가 실패');
     res.status(200).json({
       img: puzzleInfo.image,
       level: puzzleInfo.level,
@@ -16,6 +19,7 @@ const getPuzzleInfo = async (req: any, res: any, next: any) => {
 const checkURL = (req: any, res: any) => {
   let findValidURL = true;
   let randomAddress = '';
+  console.log('checkURL');
   while (findValidURL) {
     randomAddress = Math.random().toString(36).substr(2, 11);
     findValidURL = roomURL.has(randomAddress);
