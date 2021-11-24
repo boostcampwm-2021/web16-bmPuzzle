d0ae7eca97ac26eed15685cc603bcc8148fe7721
J133_유진 <susanna1221@naver.com>
2021-11-24 17:02:54 +0900

    fix: debounce 조절

--- frontend/src/components/play-puzzle/puzzle-canvas/puzzle/move-puzzle.tsx
+++ frontend/src/components/play-puzzle/puzzle-canvas/puzzle/move-puzzle.tsx
@@ -261,21 +261,21 @@ const findNearTile = (isFirstClient: boolean, socket: any, roomID: string) => {
             true,
             socket
           );
-          config.groupTiles.forEach((gtile, idx) => {
-            socket.emit("tilePosition", {
-              roomID: roomID,
-              tileIndex: idx,
-              tilePosition: gtile[0].position,
-              tileGroup: gtile[1],
-              changedData: gtile[0],
-            });
-          });
+          // config.groupTiles.forEach((gtile, idx) => {
+          //   socket.emit("tilePosition", {
+          //     roomID: roomID,
+          //     tileIndex: idx,
+          //     tilePosition: gtile[0].position,
+          //     tileGroup: gtile[1],
+          //     changedData: gtile[0],
+          //   });
+          // });
         }
       });
 
       config.groupTiles.forEach((gtile, idx) => {
         if (gtile[0] === tile) {
-          if (idx === undefined) {
+          if (gtile[1] === undefined) {
             socket.emit("tilePosition", {
               roomID: roomID,
               tileIndex: idx,
@@ -284,14 +284,15 @@ const findNearTile = (isFirstClient: boolean, socket: any, roomID: string) => {
               changedData: gtile[0],
             });
           } else {
-            config.groupTiles.forEach((tile, group) => {
-              if (idx === group) {
+            config.groupTiles.forEach((tileNow, index) => {
+              if (gtile[1] === tileNow[1]) {
+                console.log();
                 socket.emit("tilePosition", {
                   roomID: roomID,
-                  tileIndex: idx,
-                  tilePosition: gtile[0].position,
-                  tileGroup: gtile[1],
-                  changedData: gtile[0],
+                  tileIndex: index,
+                  tilePosition: tileNow[0].position,
+                  tileGroup: tileNow[1],
+                  changedData: tileNow[0],
                 });
               }
             });
