f0096211c675e2f0ad5c067057ba02d72e008b1d
betty214 <bettyjang@ewhain.net>
2021-11-24 16:50:16 +0900

    feat:debounce test3

--- frontend/src/components/play-puzzle/puzzle-canvas/puzzle/move-puzzle.tsx
+++ frontend/src/components/play-puzzle/puzzle-canvas/puzzle/move-puzzle.tsx
@@ -250,6 +250,7 @@ const findNearTile = (isFirstClient: boolean, socket: any, roomID: string) => {
           tileShape[index] = config.shapes[nextIndex];
         }
       });
+      let flag: boolean = false;
       tileArr.forEach((nowIndexTile, index) => {
         if (nowIndexTile !== undefined) {
           fitTiles(
@@ -270,15 +271,18 @@ const findNearTile = (isFirstClient: boolean, socket: any, roomID: string) => {
               changedData: gtile[0],
             });
           });
+          flag = true;
         }
       });
-      socket.emit("tilePosition", {
-        roomID: roomID,
-        tileIndex: nowIndex,
-        tilePosition: config.groupTiles[nowIndex][0].position,
-        tileGroup: config.groupTiles[nowIndex][1],
-        changedData: [event.delta.x, event.delta.y],
-      });
+      if (!flag) {
+        socket.emit("tilePosition", {
+          roomID: roomID,
+          tileIndex: nowIndex,
+          tilePosition: config.groupTiles[nowIndex][0].position,
+          tileGroup: config.groupTiles[nowIndex][1],
+          changedData: [event.delta.x, event.delta.y],
+        });
+      }
     };
   });
 };
