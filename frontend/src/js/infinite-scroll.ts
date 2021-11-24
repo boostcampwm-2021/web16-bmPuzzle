6389762f68faefcbd6dfed2ea13b28524a2ec817
Kim-Ha-Jeong <kha031821@gmail.com>
2021-11-24 18:35:51 +0900

    fix: custom hook을 이용한 infinite scroll 구현

---
+++ frontend/src/js/infinite-scroll.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+let prev = 0;
+const getItem = 10;
+
+const useInfiniteScroll = (
+  fetchCallback: Function,
+  ref: HTMLDivElement | null
+) => {
+  const [isSetting, setIsSetting] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const [cache, setCache] = useState(undefined);
+
+  const handleScroll = () => {
+    if (!ref || isDone) return;
+    const scrollHeight = ref.scrollHeight;
+    const scrollTop = ref.scrollTop;
+    const clientHeight = ref.clientHeight;
+
+    const ret = Math.floor(scrollTop) + clientHeight;
+    if (Math.abs(ret - scrollHeight) <= 2) {
+      setIsSetting(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!ref) return;
+    ref.addEventListener("scroll", handleScroll);
+    return () => ref.removeEventListener("scroll", handleScroll);
+  }, [ref, isDone]);
+
+  useEffect(() => {
+    if (!isSetting) return;
+    prev += getItem;
+    fetchCallback(prev);
+  }, [isSetting]);
+
+  return [setIsSetting, setIsDone, cache, setCache] as const;
+};
+
+export default useInfiniteScroll;
